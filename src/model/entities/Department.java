7a42fe642d9ad80944d581bf9630d711b8e52619
fernando bonilha dal fabbro <fbfabbro@gmail.com>
2020-10-15 06:38:24 -0300

    Department ComboBox

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -60,6 +60,7 @@ public class Department implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Department [id=" + id + ", name=" + name + "]";
+//		return "Department [id=" + id + ", name=" + name + "]";
+		return name;
 	}
 }
